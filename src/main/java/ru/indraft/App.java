abba5f0dca99874706b0473d6a184cb8652aa6fc
Edward Kobylyanets <kobylyanets@gmail.com>
2020-11-08 17:41:29 +0300

    add toolbar with synchronize button

--- src/main/java/ru/indraft/App.java
+++ src/main/java/ru/indraft/App.java
@@ -3,19 +3,13 @@ package ru.indraft;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.indraft.manager.DbManager;
 import ru.indraft.manager.OpenApiManager;
-import ru.indraft.service.LocaleService;
-import ru.indraft.service.OpenApiService;
 import ru.indraft.utils.FxmlUtils;
-
 
 /**
  * JavaFX App
@@ -37,28 +31,10 @@ public class App extends Application {
         }));
     }
 
-    private void handleSynchronize() {
-        OpenApiService tinkoffApiService = new OpenApiService();
-        tinkoffApiService.synchronizeStockInstruments();
-        tinkoffApiService.synchronizeOperations();
-    }
-
     @Override
     public void start(Stage stage) {
         initCleanupProcedure();
         DbManager.initDataBase();
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-        var vbox = new VBox();
-        vbox.setFillWidth(true);
-        vbox.getChildren().add(label);
-        var btn = new Button(LocaleService.getInstance().get("settings.button.title"));
-        btn.setOnAction(actionEvent -> handleSynchronize());
-        vbox.getChildren().add(btn);
-
         BorderPane borderPane = FxmlUtils.loadView(MAIN_WINDOW_PATH);
         Scene scene = null;
         if (borderPane != null) {
--- src/main/java/ru/indraft/controller/MainController.java
+++ src/main/java/ru/indraft/controller/MainController.java
@@ -1,5 +1,15 @@
 package ru.indraft.controller;
 
+import javafx.fxml.FXML;
+import ru.indraft.service.OpenApiService;
+
 public class MainController {
+
+    @FXML
+    private void handleSynchronize() {
+        OpenApiService tinkoffApiService = new OpenApiService();
+        tinkoffApiService.synchronizeStockInstruments();
+        tinkoffApiService.synchronizeOperations();
+    }
 
 }
